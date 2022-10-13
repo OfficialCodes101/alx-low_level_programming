c8fb252c052eef89de82d5e77251f35a7d067b42
Fengecy Onisoya <custofeng159@gmail.com>
2022-10-13 07:33:40 -0700

    Pays off

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Never o
+ * Return: I rule FF
+ */
+int main(void)
+{
+	int fish;
+	float cat;
+	double dog;
+	char egg;
+
+	printf("Int size: %zu bytes\n", sizeof(fish));
+	printf("Float size: %zu bytes\n", sizeof(cat));
+	printf("Double size: %zu bytes\n", sizeof(dog));
+	printf("Char size: %zu bytes\n", sizeof(egg));
+	return (0);
+}
