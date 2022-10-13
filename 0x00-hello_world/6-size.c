3820796027ed07b72c02f06e6ac46f67c3703a28
Fengecy Onisoya <custofeng159@gmail.com>
2022-10-13 07:52:51 -0700

    Dawg

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %c bytes\n");
-	printf("Size of an int: %d bytes\n");
-	printf("Size of a long int: %ld bytes\n");
-	printf("Size of a long long int: %lld bytes\n");
-	printf("Size of a float: %f bytes\n");
+	printf("Size of a char: %2d bytes\n", sizeof(char));
+	printf("Size of an int: %2d bytes\n", sizeof(int));
+	printf("Size of a long int: %2d bytes\n", sizeof(long int));
+	printf("Size of a long long int: %2d bytes\n", sizeof(long long int));
+	printf("Size of a float: %2d bytes\n", sizeof(float));
 	return (0);
 }
