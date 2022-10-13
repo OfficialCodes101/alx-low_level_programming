8effa83f74e9b862d0985bd0a75d68bbd4e9b0a1
Fengecy Onisoya <custofeng159@gmail.com>
2022-10-13 07:57:20 -0700

    Rich

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,11 @@
  */
 int main(void)
 {
-	printf("Size of a char: %2d bytes\n", sizeof(char));
-	printf("Size of an int: %2d bytes\n", sizeof(int));
-	printf("Size of a long int: %2d bytes\n", sizeof(long int));
-	printf("Size of a long long int: %2d bytes\n", sizeof(long long int));
-	printf("Size of a float: %2d bytes\n", sizeof(float));
+	printf("Size of a char: %zu bytes\n", sizeof(char));
+	printf("Size of an int: %zu bytes\n", sizeof(int));
+	printf("Size of a long int: %zu bytes\n", sizeof(long int));
+	printf("Size of a long long int: %zu bytes\n", sizeof(long long int));
+	printf("Size of a float: %zu bytes\n", sizeof(float));
+	printf("Size of a double: %zu bytes\n", sizeof(double));
 	return (0);
 }
