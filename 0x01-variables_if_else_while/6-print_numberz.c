b438dcf81b4d55022bb343ff894f9622ef582738
Fengecy Onisoya <custofeng159@gmail.com>
2022-10-14 13:57:04 -0700

    Allow

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: fish
+ */
+int main(void)
+{
+	int gee = '0';
+
+	for (gee = '0'; gee <= '9'; gee++)
+		putchar(gee);
+	putchar('\n');
+	return (0);
+}
