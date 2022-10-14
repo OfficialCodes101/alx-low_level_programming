92a730683758ba7bd727ff6364752ed49c5cc166
Fengecy Onisoya <custofeng159@gmail.com>
2022-10-14 13:47:14 -0700

    fish

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Fish
+ */
+int main(void)
+{
+	int fish = 0;
+
+	while (fish < 10)
+	{
+		printf("%i", fish);
+		fish++;
+	}
+	putchar('\n');
+	return (0);
+}
