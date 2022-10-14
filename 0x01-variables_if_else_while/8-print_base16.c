68a95c0f3d09e31b8dddfea160fd010885d6d96d
Fengecy Onisoya <custofeng159@gmail.com>
2022-10-14 15:07:25 -0700

    rocks

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - fish
+ * Return: Bigger fish
+ */
+int main(void)
+{
+	int n = '0';
+	char l = 'a';
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	while (l <= 'f')
+	{
+		putchar(l);
+		l++;
+	}
+	
+	putchar('\n');
+	return (0);
+}
