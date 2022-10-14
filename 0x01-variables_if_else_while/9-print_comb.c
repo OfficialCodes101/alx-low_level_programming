b36d6f272cd64cf785cbba42257daed0db4d5697
Fengecy Onisoya <custofeng159@gmail.com>
2022-10-14 15:17:47 -0700

    rag

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Catfish
+ */
+int main(void)
+{
+	int i = '0';
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
