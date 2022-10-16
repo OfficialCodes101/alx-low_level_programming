05d92fefe5f117f20d51e38e61f538566027326d
Fengecy Onisoya <custofeng159@gmail.com>
2022-10-16 10:18:15 -0700

    fish

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Fishes
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (b = (c + 1); b <= '9'; b++)
+		{
+			for (a = (b + 1); a <= '9'; a++)
+			{
+				putchar(c);
+				putchar(b);
+				putchar(a);
+
+				if (c != '7' || b != '8' || a != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
