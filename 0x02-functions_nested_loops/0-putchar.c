1b9cfe41da5e7ce947539b21d9e2238966eaa284
Fengecy Onisoya <custofeng159@gmail.com>
2022-10-18 03:15:33 -0700

    Moving

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void skrr(char *c);
+
+/**
+ * main - Entry point
+ * Return: Eat my hammer
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+	skrr(c);
+	putchar('\n');
+	return (0);
+}
+
+void skrr(char *c)
+{
+	int i = 0;
+	while  (c[i] != '\0')
+	{
+		putchar(c[i]);
+		i++;
+	}
+}
