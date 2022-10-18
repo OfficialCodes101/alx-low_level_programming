c539c82e53b6d1e73bf8b86e3fdcf2ce49b4e2b9
Fengecy Onisoya <custofeng159@gmail.com>
2022-10-18 04:57:24 -0700

    fish

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,12 @@ void multiply(int a, int b)
 {
 	    printf("Multiplied value=%d\n", a * b);
 }
+void print_alphabet(void)
+{
+	char x;
+
+        for (x = 'a'; x <= 'z'; x++)
+        {
+                putchar(x);
+        }
+}
