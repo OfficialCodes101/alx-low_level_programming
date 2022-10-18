40e4ca81f7963fc92b07e7be1f690d671d06582a
Fengecy Onisoya <custofeng159@gmail.com>
2022-10-18 03:21:08 -0700

    fish

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,20 @@ void skrr(char *c);
 int main(void)
 {
 	char c[] = "_putchar";
+
 	skrr(c);
 	putchar('\n');
 	return (0);
 }
 
+/**
+ * skrr - I dont even know
+ * @c: Like I know
+ */
 void skrr(char *c)
 {
 	int i = 0;
+
 	while  (c[i] != '\0')
 	{
 		putchar(c[i]);
