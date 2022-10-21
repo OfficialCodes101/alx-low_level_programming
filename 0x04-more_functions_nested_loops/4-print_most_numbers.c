69064d4121a386b08ca110ab26a962bb2f28fadf
Fengecy Onisoya <custofeng159@gmail.com>
2022-10-21 13:48:57 -0700

    Skrr

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Omorrr
+ * @void: Some shii
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+}
