4a7aa8b7947d05d0f0c4839e17538afaf7531805
Fengecy Onisoya <custofeng159@gmail.com>
2022-10-21 13:37:19 -0700

    f

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Betty winsh
+ * @void: Value
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		int _putchar(c);
+	}
+	int _putchar('\n');
+}
