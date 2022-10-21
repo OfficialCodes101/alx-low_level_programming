8a40a33ce0059c5b7f9944558875f386fe7a6c7b
Fengecy Onisoya <custofeng159@gmail.com>
2022-10-21 13:23:43 -0700

    fish

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - Check for digits
+ * @c: Lmao
+ * Return: Relative
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
