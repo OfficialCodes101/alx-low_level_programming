bc0d1d41f0c1e1545de96cde13a1c3d6f1b1bb1f
Fengecy Onisoya <custofeng159@gmail.com>
2022-10-21 13:16:08 -0700

    Cats lmao

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - I don't even know
+ * @c: Some stuff
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
