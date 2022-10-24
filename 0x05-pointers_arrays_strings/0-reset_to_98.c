511eef36b06584a3859a9ee4af29e6c57a84bef3
Fengecy Onisoya <custofeng159@gmail.com>
2022-10-24 02:22:20 -0700

    Well played

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,6 +3,7 @@
 /**
  * reset_to_98 - Swapping integers idk
  * @n: Parameter
+ * Return: nothing
  */
 void reset_to_98(int *n)
 {
@@ -10,4 +11,5 @@ void reset_to_98(int *n)
 
 	n = &a;
 	*n = 98;
+	return;
 }
