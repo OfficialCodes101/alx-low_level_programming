4b4503ae4491d5ebd881d43bfa606f7c1ce12956
Fengecy Onisoya <custofeng159@gmail.com>
2022-10-24 05:54:15 -0700

    c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _strlen - Length of string
+ * @s: char data type
+ */
+int _strlen(char *s)
+{
+	return (_strlen(*s));
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts - Printing a string
+ * @str: Parameter
+ */
+void _puts(char *str)
+{
+	str = "I am handsome asf";
+	_putchar(*str);
+}
