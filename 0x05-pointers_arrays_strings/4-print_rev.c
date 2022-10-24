e7922952390e200463ebab43bdfc835c38b97456
Fengecy Onisoya <custofeng159@gmail.com>
2022-10-24 16:46:48 -0700

    rr

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,5 +6,15 @@
  */
 void print_rev(char *s)
 {
-	revstr(s);
+	int a = 0;
+	int b;
+
+	for (b = 0; s[b] != 0; b++)
+	{
+	}
+	for (a = b - 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
 }
